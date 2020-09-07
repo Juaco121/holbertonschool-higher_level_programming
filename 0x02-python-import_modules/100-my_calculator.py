6674272fed6da404f3849d3a8745241010c0afd3
Juaco121 <juanfernandogranada@gmail.com>
2020-09-07 00:36:59 +0000

    Problem with permission

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    from calculator_1 import add, sub, div, mul
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     from sys import argv
-    args = len(argv)
-    if args != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    if len(argv) - 1 != 3:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
     if argv[2] == '+':
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        print("{} + {} = {:d}"
+              .format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
     elif argv[2] == '-':
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        print("{} - {} = {:d}"
+              .format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
     elif argv[2] == '*':
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        print("{} * {} = {:d}"
+              .format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
     elif argv[2] == '/':
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        print("{} / {} = {:d}"
+              .format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
