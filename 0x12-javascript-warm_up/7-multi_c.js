d408f2983efb16202dcde122b9d5dd42510f8adb
Juaco121 <juanfernandogranada@gmail.com>
2021-02-02 19:49:32 -0500

    Spaces

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 
 const times = parseInt(process.argv[2]);
 if (times) {
-    for (let i = 0; i < times; i++) {
-      console.log('C is fun');
-    }
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
 } else {
     console.log('Missing number of occurrences');
 }
